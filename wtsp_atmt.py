e2cb277158707ba142a9aabbfd4b8c916a965bc7
Bhargav <bhargavkadali39@gmail.com>
2021-09-30 22:45:55 +0530

    Update wtsp_atmt.py

--- wtsp_atmt.py
+++ wtsp_atmt.py
@@ -5,4 +5,4 @@ pywhatkit.sendwhatmsg('+91xxxxxxxxxx', 'Test message', 22, 33)
 # Here first provide the phn.number of person who needs the message to be send.
 # Second attribute is where you give your text that will be sent.
 # Lastly the moudle takes 24hours time so instead of 10 33 give it as 22 33 with seperated comas as above 
-# syntax: ('PHONENUMBER','MESSAGE',HOUR,MINUTE)
+# syntax: pywhatkit.sendwhatmsg('PHONENUMBER','MESSAGE',HOUR,MINUTE)
